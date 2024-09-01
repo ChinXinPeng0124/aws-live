c3a6d036a9a7ddfbd7763b02cedddc01c478c2b4
ChinXinPeng0124 <96033014+ChinXinPeng0124@users.noreply.github.com>
2024-09-01 17:31:07 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.c8raoxccz6na.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "peng-employee"
+customregion = "us-east-1"
+
